ab426f2e19e5f17c0f16c5a02a5381590c461d05
hamadayuuki <yuukirinnma@gmail.com>
2025-03-25 06:06:52 +0900

    Impl coordinate review

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import os
 import base64
+from pydantic import BaseModel
 import requests
 
 from fastapi import FastAPI
@@ -72,3 +73,56 @@ async def checkVisionGPT():
         max_tokens=300,
     )
     return {"result": response.choices[0].message.content}
+
+
+class ImageRequest(BaseModel):
+    image_base64: str
+
+@app.get("/coordinate-review")
+async def coordinateReview(request: ImageRequest):
+    imageURL = "https://images.wear2.jp/coordinate/DZiOeg3/21k0twHn/1728043950_500.jpg"   # WEARのコーデ画像
+    imageData = requests.get(imageURL).content
+    encodedImage = base64.b64encode(imageData).decode('utf-8')
+    prompt = """
+    添付する画像に合わせて、以下の質問に回答する形でコーデに関するコメントをください。
+
+    ## フォーマット
+    - ワンポイントアイテムを褒める <褒める>
+    例 : 白色のキャップが可愛いですね
+    - サイズ感についてのコメント
+    例 : ワイドなパンツを履いているのでラフでスマートな印象を受けます。\n上下のサイズがちょうど良いのでおしゃれな印象を受けます
+    - シルエット診断 <知見の共有>
+    例 : あなたのシルエットはIです。Iが似合うのは〜のような特徴を持った方です
+    - コーディネートタイプ診断 <知見の共有>
+    例 : また、あなたのコーデはカジュアルです。
+    - あなたに似合うコーディネートタイプは
+    例 : カジュアル が似合う方におすすめのコーデタイプは、ノームコアです。
+    
+    ## アウトプットのフォーマット（必ず文字列のみでアウトプットしてください）
+    <ワンポイントアイテムを褒める>\n
+    <サイズ感についてのコメント>\n
+    <シルエット診断>\n
+    <コーディネートタイプ診断>\n
+    <あなたに似合うコーディネートタイプは>\n
+    """
+    response = client.chat.completions.create(
+        model="gpt-4o-mini",
+        messages=[
+            {
+                "role": "user",
+                "content": [
+                    {"type": "text", "text": prompt},
+                    {
+                        "type": "image_url",
+                        "image_url": {
+                            "url": f"data:image/jpeg;base64,{request.image_base64}"
+                        }
+                    }
+                ]
+            }
+        ],
+        max_tokens=300,
+    )
+    return {"result": response.choices[0].message.content}
+
+
